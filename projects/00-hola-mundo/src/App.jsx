edd0d2c854760e58c3f0fe955e6ad364dbaac7ba
Alejandro Fernandez Polo <afernanpolo@gmail.com>
2025-07-08 21:32:39 +0200

    finished cards

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard.jsx';
+
+export function App() {
+
+    const users = [
+        { userName: 'luispolopolo', name: 'Luis Polo Polo', initialIsFollowing: true },
+        { userName: 'junicode', name: 'Juan Carlos', initialIsFollowing: false },
+        { userName: 'pepelote', name: 'Pepe Lote', initialIsFollowing: false }
+    ];
+    return (
+        <section className="App">
+            {users.map( user => { 
+                const {userName, name, initialIsFollowing } = user
+                return (
+                <TwitterFollowCard key={userName} userName={userName} initialIsFollowing={initialIsFollowing}>
+                    {name}
+                </TwitterFollowCard>
+                )
+            })}
+        </section>
+    )
+}
